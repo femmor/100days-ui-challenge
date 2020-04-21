468ceae7176b7fef76e56451e623efddddf42a39
Emmanuel Egomson <femmor2u@gmail.com>
2020-04-21 17:36:32 -0500

    finalised the design

--- day-2-credit-card-checkout-modal/app.js
+++ day-2-credit-card-checkout-modal/app.js
@@ -1,17 +1,38 @@
+
 const btn = document.getElementById("modalBtn");
-const modal = document.getElementById("formModal");
-const close = document.getElementById("close");
+const modal = document.getElementById("modal");
+// const btnClose = document.getElementById("close");
 
 btn.addEventListener("click", () => {
-   modal.style.display = "inline-block";
-})
-
-close.addEventListener("click", () => {
-    modal.style.display = "none";
+    modal.style.display = "block";
 })
 
 window.onclick = (e) => {
-    if (e.target == modal) {
+    if (e.target === modal) {
         modal.style.display = "none";
     }
 }
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    const slides = document.getElementsByClassName("cardSlide");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+  }
